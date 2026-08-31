7b989f1bf0b81ed3f5f9913dfdb7975cc16d5c9c
Vedant Aware <vedaware33@gmail.com>
2026-08-31 22:55:07 +0530

    Connect Android app to live NEXUS web app

--- app/src/main/java/com/ved/nexus/MainActivity.java
+++ app/src/main/java/com/ved/nexus/MainActivity.java
@@ -5,21 +5,26 @@ import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.view.Window;
 
 public class MainActivity extends Activity {
   @Override public void onCreate(Bundle state) {
     super.onCreate(state);
+    requestWindowFeature(Window.FEATURE_NO_TITLE);
     WebView w = new WebView(this);
     w.setWebViewClient(new WebViewClient());
     WebSettings s = w.getSettings();
     s.setJavaScriptEnabled(true);
     s.setDomStorageEnabled(true);
     s.setDatabaseEnabled(true);
-    s.setAllowFileAccess(true);
-    s.setAllowContentAccess(true);
     s.setSupportZoom(false);
-    w.loadUrl("file:///android_asset/index.html");
+    s.setBuiltInZoomControls(false);
+    s.setDisplayZoomControls(false);
+    w.loadUrl("https://nexusbyed.netlify.app/");
     setContentView(w);
   }
-  @Override public void onBackPressed() { super.onBackPressed(); }
+  @Override public void onBackPressed() {
+    WebView w = (WebView) findViewById(android.R.id.content);
+    super.onBackPressed();
+  }
 }
